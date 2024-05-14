f231c11e7c9515e994674b100c1179b7cd66d69e
ArtyWhit <julwer1k@gmail.com>
2024-05-14 15:46:54 +0300

    make build

---
+++ index.a6abb591.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientWidth,l=e.clientHeight,i=t.getBoundingClientRect(),n=(c-i.width)/2,o=(l-i.height)/2;t.style.top="".concat(n,"px"),t.style.left="".concat(o,"px");
+//# sourceMappingURL=index.a6abb591.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+// write code here
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+const spiderSize = spider.getBoundingClientRect();
+const x = (wallWidth - spiderSize.width) / 2;
+const y = (wallHeight - spiderSize.height) / 2;
+spider.style.top = `${x}px`;
+spider.style.left = `${y}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
